cb4e0b1fb1217682518f5091867561f27f75f777
taltekc <taltekc@gmail.com>
2018-10-14 13:20:17 -0400

    dynmaicPath

--- src/facebookTest.java
+++ src/facebookTest.java
@@ -7,8 +7,12 @@ public class facebookTest {
 
 	public static void main(String[] args) {
 
-		System.setProperty("webdriver.chrome.driver", "C://Users//talentTEK//Documents//GitHub//SeleniumFramework//Drivers//chromedriver.exe");
-		// dynamic path
+		// Dynamic Path
+		//System.setProperty("webdriver.chrome.driver", "C://Users//talentTEK//Documents//GitHub//SeleniumFramework//Drivers//chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
+	
+		
+		
 		// OS
 		WebDriver driver = new ChromeDriver();
 		// Different browsers
