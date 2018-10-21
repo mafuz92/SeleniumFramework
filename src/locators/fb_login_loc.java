ad8c8bdba4e5a502c93f58808b39d006d890ced3
taltekc <taltekc@gmail.com>
2018-10-21 12:23:30 -0400

    framework2

--- src/locators/fb_login_loc.java
+++ src/locators/fb_login_loc.java
@@ -5,4 +5,5 @@ public class fb_login_loc {
 	public String emailPhone 	 	= "//input[@id='email']";
 	public String password 			= "//input[@type='password']";
 	public String loginButton 		= "//*[@id='u_0_3']";
+	
 }
--- src/utils/SWDFunctions.java
+++ src/utils/SWDFunctions.java
@@ -12,6 +12,14 @@ public class SWDFunctions {
 	public void typeByXpath (String locator, String value){
 		driver.findElement (By.xpath(locator)).sendKeys (value);
 	}
+	public void typeByCss(String locator, String value){
+		driver.findElement (By.cssSelector(locator)).sendKeys (value);
+	}
+	public void typeById(String locator, String value){
+		driver.findElement (By.id(locator)).sendKeys (value);
+	}
+	
+	
 	// to click
 	public void clickByXpath (String locator){
 		driver.findElement(By.xpath (locator)).click();
