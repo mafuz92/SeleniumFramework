06ead9fa1e13726ffd36f791c245dcc584b2d2d2
taltekc <taltekc@gmail.com>
2018-10-28 11:30:54 -0400

    Skip test

--- src/base/config.java
+++ src/base/config.java
@@ -1,23 +1,44 @@
 package base;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+
 import utils.SWDFunctions;
 
 
 public class config extends SWDFunctions {
 
-	// cross-browser 
-	// driver quite and close
-
-	@BeforeTest
-	public void beforeTestStart(){
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//Drivers//chromedriver.exe");	
-		driver = new ChromeDriver();
-		
-		driver.get("https://www.facebook.com");
-		
-		// 
+	@BeforeSuite
+	public void beforeSuite(){
+		// db conntion
+		// suite start time
 	}
 
+	@BeforeMethod
+	public void setUp(){
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//Drivers//chromedriver.exe");	
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+	}
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.close();
+	}
+
+	@AfterTest
+	public void closeDriver(){
+		driver.quit();
+	}
+	
+	@AfterSuite
+	public void afterSuite(){
+		// db conntion
+		// suite end time
+	}
 }
